27e064dedae0c94c5e3ac23bb0e49d1c7c24c8b4
LeonRivera <leonriv2@gmail.com>
2022-08-19 23:32:53 -0500

    19082022 | LeonRivera | Added web.config to deploy

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -32,3 +32,22 @@ VALUES ('female', 'Aline', 1, 'https://www.pngmart.com/files/10/User-Account-PNG
 
 INSERT INTO email (email) VALUES ('aline@gmail.com');
 INSERT INTO user_emails (user_id, emails_id) VALUES (1,1);
+INSERT INTO email (email) VALUES ('aline@gmail.com');
+INSERT INTO user_emails (user_id, emails_id) VALUES (2,2);
+INSERT INTO email (email) VALUES ('aline@gmail.com');
+INSERT INTO user_emails (user_id, emails_id) VALUES (3,3);
+INSERT INTO email (email) VALUES ('aline@gmail.com');
+INSERT INTO user_emails (user_id, emails_id) VALUES (4,4);
+INSERT INTO email (email) VALUES ('aline@gmail.com');
+INSERT INTO user_emails (user_id, emails_id) VALUES (5,5);
+INSERT INTO email (email) VALUES ('aline@gmail.com');
+INSERT INTO user_emails (user_id, emails_id) VALUES (6,6);
+INSERT INTO email (email) VALUES ('aline@gmail.com');
+INSERT INTO user_emails (user_id, emails_id) VALUES (7,7);
+INSERT INTO email (email) VALUES ('aline@gmail.com');
+INSERT INTO user_emails (user_id, emails_id) VALUES (8,8);
+INSERT INTO email (email) VALUES ('aline@gmail.com');
+INSERT INTO user_emails (user_id, emails_id) VALUES (9,9);
+INSERT INTO email (email) VALUES ('aline@gmail.com');
+INSERT INTO user_emails (user_id, emails_id) VALUES (10,10);
+
